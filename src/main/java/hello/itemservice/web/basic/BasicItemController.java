610e2bf572a2b043bc34d1e274173b46f1f6549f
andrew <statco19@gmail.com>
2022-01-26 16:10:58 +0900

    created addItemV2

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -44,7 +44,7 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV1(@RequestParam String itemName,
                             @RequestParam Integer price,
                             @RequestParam Integer quantity,
@@ -55,6 +55,14 @@ public class BasicItemController {
 
         model.addAttribute("item", item);
 
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+//        model.addAttribute("item", item);
+        
         return "basic/item";
     }
 
